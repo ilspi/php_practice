24396158abc716a2f2d7d1f2ed4f871e43b46178
Ilya Spiridonov <115345247+ilspi@users.noreply.github.com>
2022-11-01 20:09:57 +0300

    Delete practice directory

--- practice/logo.inc.php
+++
@@ -1,5 +0,0 @@
-    
-                <div class="logo"> 
-                <img src="\practice\img\php_PNG26.png"" alt="php">
-                </div>                                                 
-        
